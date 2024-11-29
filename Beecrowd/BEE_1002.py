949d3d2886b39889254ad8384ae210de8e17b3b7
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2024-11-29 17:43:14 +0600

    BEE_1002

---
+++ Beecrowd/BEE_1002.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+"""
+
+n = 3.14159
+R = float(input())
+
+A = n * (R**2)
+
+print("A=%.4f" % A)
