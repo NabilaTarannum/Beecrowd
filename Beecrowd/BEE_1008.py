463dde0c9e9b72bf914775ea16dd98864bcecafd
Nabila Tarannum <89971373+NabilaTarannum@users.noreply.github.com>
2024-12-10 23:50:18 +0600

    BEE_1008

---
+++ Beecrowd/BEE_1008.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+"""
+
+number = int(input())
+worked_hours = int(input())
+amount_per_hour = float(input())
+
+salary = worked_hours * amount_per_hour
+
+print("NUMBER =", number)
+print("SALARY = U$ %.2f" % salary)
